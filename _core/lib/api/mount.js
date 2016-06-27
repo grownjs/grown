54410e9705c75d9f08167cbd7134607a51385f42
Alvaro Cabrera <pateketrueke@gmail.com>
2016-06-27 18:50:46 -0500

    Register callbacks in order; fix

--- _core/lib/api/mount.js
+++ _core/lib/api/mount.js
@@ -2,7 +2,7 @@ var buildFactory = require('../factory');
 
 module.exports = function (context, container) {
   context.mount = function (callback) {
-    container.pipeline.unshift(buildFactory(callback, container.options));
+    container.pipeline.push(buildFactory(callback, container.options));
 
     return context;
   };
