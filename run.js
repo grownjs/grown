793dc2ed5240ff4a90b7ec15f1d6584037cf7323
Alvaro Cabrera <pateketrueke@gmail.com>
2016-06-28 04:49:21 -0500

    Fixed example

--- run.js
+++ run.js
@@ -1,7 +1,23 @@
-var server = require('./_core')({
-  cwd: process.cwd()
-});
-server.use(require('./_core/router'));
+var server = require('./_core')();
 var app = server.listen(8000);
-// console.log(server);
+
+// x-response-time
+server.mount(function xResponseTime(conn) {
+  var _start = new Date();
+
+  return conn.next(function () {
+    var ms = (new Date()) - _start;
+
+    conn.set('x-response-time', ms + 'ms');
+  });
+});
+
+server.use(require('./_core/router')(process.cwd()));
+
+server.mount(function MyApp(conn) {
+  if (conn.req.method === 'GET' && conn.req.url === '/x') {
+    conn.body = (conn.body || ':O').replace(':)', ':D');
+  }
+});
+
 console.log('Listening at ' + app.location.href);
