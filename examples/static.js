fcd5525aa929c6671162282827bad0a393ab5ef8
Alvaro Cabrera <pateketrueke@gmail.com>
2016-07-14 00:46:19 -0500

    Fixed serve-index example for static files

--- examples/static.js
+++ examples/static.js
@@ -1,6 +1,7 @@
 var server = require('homegrown')();
 
 server.mount(require('serve-static')(__dirname));
+server.mount(require('serve-index')(__dirname));
 
 server.listen(5000, function (app) {
   console.log('Listening on', app.location.href);
