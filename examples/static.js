ac1db4c9c174aaf1955162ed2ace59d507caa349
Alvaro Cabrera <pateketrueke@gmail.com>
2016-09-22 00:59:45 -0500

    Fixed example

--- examples/static.js
+++ examples/static.js
@@ -2,8 +2,8 @@
 
 const server = require('..')();
 
-server.mount(require('serve-static')(__dirname));
 server.mount(require('serve-index')(__dirname));
+server.mount(require('serve-static')(__dirname));
 
 server.listen(5000, (app) => {
   console.log('Listening on', app.location.href);
