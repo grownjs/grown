e76d02306cc41f25251b31759ed3691fb1f579a1
Alvaro Cabrera <pateketrueke@gmail.com>
2016-12-03 04:13:14 -0600

    Fixed handlers; kill-em-all

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const _env = require('dotenv');
 
 const _farms = [];
 
+function _closeAll() {
+  _farms.forEach((farm) => {
+    Object.keys(farm._context.hosts).forEach((host) => {
+      farm._context.hosts[host].close();
+    });
+  });
+}
+
+// gracefully dies
+process.on('exit', _closeAll);
+process.on('SIGINT', () => process.exit());
+
 const useFactory = require('./lib/api/use');
 const mountFactory = require('./lib/api/mount');
 const listenFactory = require('./lib/api/listen');
@@ -34,35 +46,38 @@ module.exports.new = (options) => {
     value: (_opts) => _env.config(_opts || { silent: true }),
   });
 
+  function _dispatch(err, conn) {
+    /* istanbul ignore else */
+    if (conn.res._hasBody && conn.res._headerSent) {
+      conn.res.end();
+      return;
+    }
+
+    /* istanbul ignore else */
+    if (!conn.res.finished || !err) {
+      if (conn.body === null && conn.res.statusCode === 200) {
+        const errObj = err || new Error('Not Implemented');
+
+        errObj.statusMessage = errObj.statusMessage || errObj.message;
+        errObj.statusCode = errObj.statusCode || 501;
+
+        throw errObj;
+      } else {
+        conn.send(conn.body);
+      }
+    }
+  }
+
   Object.defineProperty(container, '_dispatch', {
     configurable: false,
     enumerable: false,
     writable: false,
-    value: pipelineFactory('_dispatch', container.pipeline, (err, conn) => {
-      /* istanbul ignore else */
-      if (conn.res._hasBody && conn.res._headerSent) {
-        conn.res.end();
-        return;
-      }
-
-      /* istanbul ignore else */
-      if (!conn.res.finished || !err) {
-        if (conn.body === null && conn.res.statusCode === 200) {
-          const errObj = err || new Error('Not Implemented');
-
-          errObj.statusMessage = errObj.statusMessage || errObj.message;
-          errObj.statusCode = errObj.statusCode || 501;
-
-          throw errObj;
-        } else {
-          conn.send(conn.body);
-        }
-      }
-    }),
+    value: pipelineFactory('_dispatch', container.pipeline, _dispatch),
   });
 
   return container._context;
 };
 
+module.exports.burn = _closeAll;
 module.exports.farms = cb => _farms.forEach(cb);
 module.exports.version = require('./package.json').version;
