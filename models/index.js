22871bf19bfbddd4eae09bb100302184c1435f20
Alvaro Cabrera <pateketrueke@gmail.com>
2016-12-04 03:56:27 -0600

    Improved models integration

--- models/index.js
+++ models/index.js
@@ -20,48 +20,54 @@ function _model(name, props, $schema, sequelize) {
 }
 
 function _hook(cwd) {
-  return (container) => {
-    container.extensions.models = {};
+  /* istanbul ignore else */
+  if (typeof cwd !== 'string' || !fs.existsSync(cwd)) {
+    throw new Error(`Expecting 'cwd' to be a valid directory, given '${cwd}'`);
+  }
 
-    /* istanbul ignore else */
-    if (typeof cwd !== 'string' || !fs.existsSync(cwd)) {
-      throw new Error(`Expecting 'cwd' to be a valid directory, given '${cwd}'`);
+  const _defaults = require(path.join(cwd, 'config', 'database.js'));
+  const _environment = process.env.NODE_ENV || 'dev';
+  const _options = _defaults[_environment];
+  const _config = _options || _defaults;
+  const _models = {};
+  const _tasks = [];
+
+  // db-migrate
+  _config.driver = _config.dialect;
+  _config.filename = _config.storage;
+
+  const _sequelize = new Sequelize(_config);
+
+  glob.sync('models/**/*.js', { cwd, nodir: true }).forEach((model) => {
+    const definition = require(path.join(cwd, model));
+    const $schema = definition.$schema || {};
+
+    delete definition.$schema;
+
+    const modelName = $schema.id || path.relative('models', model)
+      .replace(/(index)?\.js/, '')
+      .replace(/Model(\/|$)/g, '');
+
+    const tableName = definition.table || modelName
+      .replace(/[A-Z]/g, ($0) => `_${$0}`)
+      .replace(/^_/, '')
+      .toLowerCase();
+
+    _models[modelName] = _model(tableName, definition, $schema, _sequelize);
+    _models[modelName].modelName = modelName;
+
+    if (_environment !== 'prod') {
+      _tasks.push(() => _models[modelName].sync());
     }
+  });
 
-    const _defaults = require(path.join(cwd, 'config', 'database.js'));
-    const _environment = process.env.NODE_ENV || 'dev';
-    const _options = _defaults[_environment];
-    const _config = _options || _defaults;
-    const _models = {};
-    const _tasks = [];
+  return (container) => {
+    const _map = container.extensions.models = {};
 
-    // db-migrate
-    _config.driver = _config.dialect;
-    _config.filename = _config.storage;
+    _map.map = cb => Object.keys(_models).map(k => cb(_models[k]));
+    _map.forEach = cb => Object.keys(_models).forEach(k => cb(_models[k]));
 
-    const _sequelize = new Sequelize(_config);
-
-    glob.sync('models/**/*.js', { cwd, nodir: true }).forEach((model) => {
-      const definition = require(path.join(cwd, model));
-      const $schema = definition.$schema || {};
-
-      delete definition.$schema;
-
-      const modelName = $schema.id || path.relative('models', model)
-        .replace(/(index)?\.js/, '')
-        .replace(/Model(\/|$)/g, '');
-
-      const tableName = definition.table || modelName
-        .replace(/[A-Z]/g, ($0) => `_${$0}`)
-        .replace(/^_/, '')
-        .toLowerCase();
-
-      _models[modelName] = _model(tableName, definition, $schema, _sequelize);
-
-      if (_environment !== 'prod') {
-        _tasks.push(() => _models[modelName].sync());
-      }
-
+    Object.keys(_models).forEach((modelName) => {
       Object.defineProperty(container.extensions.models, modelName, {
         configurable: false,
         enumerable: true,
