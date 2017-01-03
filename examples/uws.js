97bfab4b3d5fbb9284c65212cd0f6d822248ecc2
Alvaro Cabrera <pateketrueke@gmail.com>
2017-01-03 09:51:13 -0600

    Fixed uws example; WIP

--- examples/uws.js
+++ examples/uws.js
@@ -9,10 +9,9 @@
 // yarn example uws
 
 const uws = require('../external/uWebSockets/nodejs/dist/uws');
+const $ = require('..').new();
 
-const server = require('..').new();
-
-server.protocols.uws = {
+$.protocols.uws = {
   globalAgent: {
     defaultPort: process.env.PORT,
   },
@@ -23,57 +22,41 @@ server.protocols.uws = {
       _options = null;
     }
 
-    return {
-      listen(port, host, callback) {
-        const wss = new uws.Server({ nativeHttp: true, port: port });
-
-        wss.on('connection', (ws) => {
-          ws.terminate();
-        });
-
-        wss.on('error', (error) => {
-          console.log('Cannot start server');
-        });
-
-        wss.onHttpRequest((socket, req) => {
-          try {
-            _client({
-              url: req.url,
-              body: null,
-              method: req.verb === uws.HTTP_GET ? 'GET' : 'POST',
-              headers: {
-                host: 'localhost:5000',
-              },
-              getHeader(arg1) { req.getHeader(arg1); },
-              setHeader(arg1, arg2) {},
-            }, {
-              end(arg1) { socket.end(arg1); },
-              send(arg1) { socket.send(arg1); },
-              getHeader(arg1) {},
-              setHeader(arg1, arg2) {},
-              finished: false,
-              statusCode: 502,
-              statusMessage: 'Not Implemented',
-            }, () => console.log('DONE'));
-          } catch (e) {
-            console.log('ERR', e);
-          }
-        });
-
-        callback();
-      },
-    };
+    return uws.http.createServer((req, res) => {
+      try {
+        _client({
+          url: req.url,
+          body: null,
+          method: req.verb === uws.HTTP_GET ? 'GET' : 'POST',
+          headers: {
+            host: 'localhost:5000',
+          },
+          getHeader(arg1) { req.getHeader(arg1); },
+          setHeader(arg1, arg2) {},
+        }, {
+          end(arg1) { res.end(arg1); },
+          send(arg1) { res.send(arg1); },
+          getHeader(arg1) {},
+          setHeader(arg1, arg2) {},
+          finished: false,
+          statusCode: 502,
+          statusMessage: 'Not Implemented',
+        }, () => console.log('DONE'));
+      } catch (e) {
+        console.log('ERR', e);
+      }
+    });
   },
 };
 
-server.mount((conn) => {
+$.ctx.mount((conn) => {
   const start = new Date();
 
   return conn.next(() => {
-    conn.body = `OK in ${(new Date() - start) / 1000}ms`;
+    conn.resp_body = `OK in ${(new Date() - start) / 1000}ms`;
   });
 });
 
-server.listen('uws://localhost:5000', (app) => {
+$.ctx.listen('uws://localhost:5000', (app) => {
   console.log('Listening on', app.location.href);
 });
