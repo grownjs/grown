04bf404098d652d9f0afca5e09fbcfca287641c0
Alvaro Cabrera <pateketrueke@gmail.com>
2017-01-07 23:56:29 -0600

    Fixed error handling

--- src/conn/host.js
+++ src/conn/host.js
@@ -2,6 +2,50 @@ const connFactory = require('./ctx');
 
 const statusCodes = require('http').STATUS_CODES;
 
+// html-safe values
+function _encode(str) {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
+
+// normalize error object
+function _fixError(e) {
+  const _stack = (e.stack || '').replace(/.*Error:.+?\n/, '');
+  const _error = [e.message || e.toString()];
+  const _lines = _error[0].split('\n');
+
+  /* istanbul ignore else */
+  if (e.parent) {
+    _error.push(e.parent.message || e.parent.toString());
+  }
+
+  /* istanbul ignore else */
+  if (_stack) {
+    _error.push(_stack.split(_lines[_lines.length - 1])[1] || _stack);
+  }
+
+  const cwd = process.cwd();
+
+  const lines = _error.map((_msg) => {
+    while (_msg.indexOf(cwd) > -1) {
+      _msg = _msg.replace(cwd, '.');
+    }
+
+    return _msg;
+  });
+
+  return {
+    stack: lines.pop(),
+    body: lines,
+    name: e.name || 'Error',
+    code: e.statusCode || 500,
+    call: `∴ ${e.pipeline.join(' → ')}`,
+  };
+}
+
 module.exports = ($, protocol) => {
   return (req, res, next) => {
     // normalize
@@ -30,39 +74,13 @@ module.exports = ($, protocol) => {
         return;
       }
 
-      let _msg = e.message || e.toString();
-
-      _msg = `${e.name || 'Error'}<${e.pipeline.join(' ')}>:\n- ${_msg}`;
-
-      const _stack = (e.stack || '').replace(/.*Error:.+?\n/, '');
-      const _lines = _msg.split('\n');
+      e = _fixError(e);
 
       // normalize response
-      res.statusCode = e.statusCode || 500;
-      res.statusMessage = statusCodes[res.statusCode];
+      res.statusCode = e.code;
+      res.statusMessage = statusCodes[e.code];
 
-      res.writeHead(res.statusCode, {
-        'Content-Type': 'text/plain',
-      });
-
-      /* istanbul ignore else */
-      if (e.parent) {
-        _msg += `\n- ${e.parent.message || e.parent.toString()}`;
-      }
-
-      /* istanbul ignore else */
-      if (_stack) {
-        _msg += `\n${_stack.split(_lines[_lines.length - 1])[1] || _stack}`;
-      }
-
-      const cwd = process.cwd();
-
-      while (_msg.indexOf(cwd) > -1) {
-        _msg = _msg.replace(cwd, '.');
-      }
-
-      res.write(_msg);
-      res.end();
+      return e;
     }
 
     // lookup opened connection
@@ -76,7 +94,44 @@ module.exports = ($, protocol) => {
 
       try {
         $.ctx.dispatch(conn, $.opts)
-          .catch(err => fail(err))
+          .catch((err) => {
+            const _err = fail(err);
+
+            let _type;
+            let _msg;
+
+            switch (conn.accept.type('html', 'json')) {
+              case 'html':
+                _type = 'text/html';
+                _msg = [
+                  `<h3>${_err.name}<code>${_err.call}</code></h3><details>`,
+                  `<summary>${_encode(_err.body.shift())}</summary><pre>`,
+                  _err.body.length ? `- ${_err.body.join('\n- ')}\n` : '',
+                  `${_encode(_err.stack)}</pre></details>`,
+                ].join('');
+                break;
+
+              case 'json':
+                _type = 'application/json';
+                _msg = JSON.stringify({
+                  status: 'error',
+                  message: _err,
+                });
+                break;
+
+              default:
+                _type = 'text/plain';
+                _msg = `${_err.name} ${_err.call}\n- ${_err.body.join('\n- ')}\n${_err.stack}`;
+                break;
+            }
+
+            res.writeHead(res.statusCode, {
+              'Content-Type': _type,
+            });
+
+            res.write(_msg);
+            res.end();
+          })
           .then(() => _next());
       } catch (e) {
         fail(e, conn);
