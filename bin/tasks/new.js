80984a3ec45e3399d4cd331664bc1c841dde0391
Alvaro Cabrera <pateketrueke@gmail.com>
2017-03-01 04:00:53 -0600

    Fixed CLI

--- bin/tasks/new.js
+++ bin/tasks/new.js
@@ -2,26 +2,36 @@
 
 /* eslint-disable global-require */
 
+const _ = require('../lib/util');
+
 const path = require('path');
+const chalk = require('chalk');
 
 module.exports = $ => {
+  const IS_DEBUG = $.flags.debug === true;
+
   const Haki = require('haki');
 
   const haki = new Haki($.flags);
 
-  haki.setGenerator('app', {
+  haki.runGenerator({
     basePath: path.resolve(__dirname, '../skel'),
+    abortOnFail: true,
     prompts: [{
       name: 'appName',
-      message: 'Application name:',
+      message: 'Application name',
       validate: value => value.length > 0 || 'Missing name?',
     }],
     actions: [{
       type: 'copy',
-      srcPath: 'templates/skel',
+      srcPath: 'templates/example',
       destPath: '{{snakeCase appName}}',
     }],
+  }, {
+    appName: $._.shift(),
+  })
+  .catch(err => {
+    _.echo(chalk.red((IS_DEBUG && err.stack) || err.message), '\n');
+    _.die(1);
   });
-
-  haki.runGenerator('app', { appName: $._.shift() });
 };
