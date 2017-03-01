4c2c04645b5fcf968d8732bcbd1dac50e1c6bf06
Alvaro Cabrera <pateketrueke@gmail.com>
2017-03-01 14:30:59 -0600

    More generator fun

--- bin/skel/generate/index.js
+++ bin/skel/generate/index.js
@@ -6,7 +6,51 @@ module.exports = {
 };
 `;
 
+const TARIMA_SCRIPTS_TEMPLATE = `"dev": "tarima -ws",
+    "dist": "tarima -fq",
+    "dist:ci": "tarima -fqe ci",
+    "dist:prod": "tarima -fqe prod",
+    "dist:stage": "tarima -fqe stage",`;
+
+const TARIMA_CONFIG_TEMPLATE = `"tarima": {
+    "src": "src/**/*",
+    "ignoreFiles": [
+      ".gitignore"
+    ],
+    "devPlugins": [
+      "tarima-lr"
+    ],
+    "bundleOptions": {
+      "bundleCache": true,
+      "entryCache": true,
+      "extensions": {
+        "js": "es6",
+        "css": "less"
+      }
+    }
+  },`;
+
 module.exports = haki => {
+  haki.setGenerator('tarima:config', {
+    actions: [{
+      type: 'modify',
+      unless: '"tarima":',
+      pattern: '"dependencies":',
+      destPath: 'package.json',
+      template: `${TARIMA_CONFIG_TEMPLATE}\n  $&`,
+    }],
+  });
+
+  haki.setGenerator('tarima:tasks', {
+    actions: [{
+      type: 'modify',
+      unless: '"dev":',
+      pattern: '"scripts":\\s*\\{',
+      destPath: 'package.json',
+      template: `$&\n    ${TARIMA_SCRIPTS_TEMPLATE}`,
+    }],
+  });
+
   haki.setGenerator('controller', {
     arguments: ['controllerName'],
     prompts: [{
@@ -34,6 +78,7 @@ module.exports = haki => {
     }],
     actions: [{
       type: 'modify',
+      unless: '{{snakeCase actionName}}(',
       pattern: 'methods\\s*:\\s*\\{',
       template: '$&\n    {{snakeCase actionName}}() {},',
       destPath: 'app/controllers/{{paramCase controllerName}}.js',
---
+++ bin/skel/templates/example/app/views/layouts/default.js
@@ -0,0 +1,9 @@
+module.exports = `<!doctype html>
+<html>
+  <head>
+    <title>Untitled</title>
+  </head>
+  <body>
+    <h1>It works!</h1>
+  </body>
+</html>`;
