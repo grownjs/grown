3decdae1fed7d0e21ab034240ece9bb64d847c7c
Alvaro Cabrera <pateketrueke@gmail.com>
2017-03-01 05:20:16 -0600

    Fixing CLI tasks

---
+++ bin/skel/generate/index.js
@@ -0,0 +1,42 @@
+const CONTROLLER_TEMPLATE = `/* {{controllerName}} */
+module.exports = {
+  methods: {
+    index() {},
+  },
+};
+`;
+
+module.exports = haki => {
+  haki.setGenerator('controller', {
+    arguments: ['controllerName'],
+    prompts: [{
+      name: 'controllerName',
+      message: 'Controller name',
+      validate: value => value.length > 0 || "Don't forget name your controller",
+    }],
+    actions: [{
+      type: 'add',
+      template: CONTROLLER_TEMPLATE,
+      destPath: 'app/controllers/{{paramCase controllerName}}.js',
+    }],
+  });
+
+  haki.setGenerator('action', {
+    arguments: ['controllerName', 'actionName'],
+    prompts: [{
+      name: 'controllerName',
+      message: 'Controller name',
+      validate: value => value.length > 0 || "Don't forget name your controller",
+    }, {
+      name: 'actionName',
+      message: 'Action name',
+      validate: value => value.length > 0 || "Don't forget name your action",
+    }],
+    actions: [{
+      type: 'modify',
+      pattern: 'methods\\s*:\\s*\\{',
+      template: '$&\n    {{snakeCase actionName}}() {},',
+      destPath: 'app/controllers/{{paramCase controllerName}}.js',
+    }],
+  });
+};
--- bin/tasks/generate.js
+++ bin/tasks/generate.js
@@ -2,7 +2,7 @@
 
 /* eslint-disable global-require */
 
-module.exports = (_cmd, $) => {
+module.exports = $ => {
   const IS_DEBUG = $.flags.debug === true;
 
   // let the user define the cwd outside
@@ -10,65 +10,27 @@ module.exports = (_cmd, $) => {
 
   const _ = require('../lib/util');
 
+  const _cmd = $._.shift();
+
   const Haki = require('haki');
-  const path = require('path');
   const chalk = require('chalk');
 
-  const haki = new Haki($.flags);
+  const haki = new Haki(_.merge({}, $.flags, { data: $._ }));
 
-  const _base = {
-    env: process.env.NODE_ENV,
-    is: {
-      ci: process.env.NODE_ENV === 'ci',
-      dev: process.env.NODE_ENV === 'dev',
-      prod: process.env.NODE_ENV === 'prod',
-      stage: process.env.NODE_ENV === 'stage',
-    },
-  };
+  haki.load(require.resolve('../skel/generate'));
 
-  haki.load(require.resolve($.flags.template || '../skel/app'));
+  function _onError(err) {
+    _.echo(chalk.red((IS_DEBUG && err.stack) || err.message), '\n');
+    _.die(1);
+  }
 
-  function _showDetails(err, result) {
-    if (err) {
-      _.echo(chalk.red((IS_DEBUG && err.stack) || err.message), '\n');
-      _.die(1);
-      return;
+  function _run() {
+    if (!$.flags.list && _cmd) {
+      return haki.runGenerator(_cmd, _.merge({}, $.data, $.params));
     }
 
-    result.changes.forEach((f) => {
-      _.echo(chalk.green(`✔ ${f.type}`), ' ', chalk.yellow(path.relative($.flags.cwd, f.destPath)), '\n');
-    });
-
-    result.failures.forEach((f) => {
-      _.echo(chalk.red(`✘ ${f.type}`), ' ', chalk.yellow(path.relative($.flags.cwd, f.destPath)), '\n');
-      _.echo(chalk.red(`  ${(IS_DEBUG && f.error.stack) || f.error.toString()}`), '\n');
-    });
-
-    if (!(result.error || result.failures.length)) {
-      _.echo(chalk.green('✔ Done.\n'));
-    } else {
-      _.echo(chalk.red(`${result.error
-        ? result.error.message
-        : 'Try again with --force to apply the changes'}\n`));
-      _.die(1);
-    }
+    return haki.chooseGeneratorList(_.merge({}, $.data, $.params));
   }
 
-  function _executeTask() {
-    haki.runGenerator(_cmd, _.merge({}, _base, $.data, $.params))
-      .then(result => _showDetails(undefined, result))
-      .catch(_showDetails);
-  }
-
-  function _showTasks() {
-    haki.chooseGeneratorList(_.merge({}, _base, $.data, $.params))
-      .then(result => _showDetails(undefined, result))
-      .catch(_showDetails);
-  }
-
-  if (!$.flags.list && _cmd) {
-    _executeTask();
-  } else {
-    _showTasks();
-  }
+  _run().catch(_onError);
 };
