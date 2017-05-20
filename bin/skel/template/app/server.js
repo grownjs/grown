768f2a51706d019e5150df96e947d53dc0d81bd6
Alvaro Cabrera <pateketrueke@gmail.com>
2017-05-20 16:46:03 -0500

    Fixed default server

--- bin/skel/template/app/server.js
+++ bin/skel/template/app/server.js
@@ -17,20 +17,6 @@ module.exports = () => {
 
     // environment
     env: process.env.NODE_ENV || 'development',
-
-    // other settings
-    session: {
-      secret: process.env.SESSION_SECRET || 'secret*value',
-      keys: (process.env.SESSION_KEYS || 'secret*value').split(/\s+/),
-      maxAge: parseInt(process.env.SESSION_MAXAGE || 0, 10) || 86400000,
-    },
-    upload: {
-      multiple: process.env.UPLOAD_MULTIPLE === 'true' || true,
-      maxFiles: parseInt(process.env.UPLOAD_MAXFILES, 0) || 10,
-    },
-    logger: {
-      chalkize: process.env.LOGGER_COLORIZE === 'true' || true,
-    },
   });
 
   // enable file server
@@ -40,20 +26,37 @@ module.exports = () => {
   $.use(Grown.plugs.models(cwd));
 
   // {{/DATABASE}}load routes and views
-  $.use(Grown.plugs.router(cwd));
-  $.use(Grown.plugs.render(cwd));
+  $.use(Grown.plugs.router({
+    middlewares: {
+      settings: path.join(cwd, 'config/middlewares.js'),
+      folders: path.join(cwd, 'boot/middlewares'),
+    },
+    settings: path.join(cwd, 'config/routes.js'),
+    folders: path.join(cwd, 'app/controllers'),
+  }));
+
+  $.use(Grown.plugs.render({
+    folders: path.join(cwd, 'app/views'),
+  }));
 
   // required for CSRF
-  $.use(Grown.plugs.session($.get('session')));
+  $.use(Grown.plugs.session({
+    secret: process.env.SESSION_SECRET || 'secret*value',
+    keys: (process.env.SESSION_KEYS || 'secret*value').split(/\s+/),
+    maxAge: parseInt(process.env.SESSION_MAXAGE || 0, 10) || 86400000,
+  }));
 
   // enable file uploads
-  $.use(Grown.plugs.upload($.get('upload')));
+  $.use(Grown.plugs.formidable({
+    multiple: process.env.UPLOAD_MULTIPLE === 'true' || true,
+    maxFiles: parseInt(process.env.UPLOAD_MAXFILES, 0) || 10,
+  }));
 
   // inject logging helpers
   $.use(Grown.plugs.logger({
     transports: [{
       Console: {
-        chalkize: $.get('logger.chalkize'),
+        chalkize: process.env.LOGGER_COLORIZE === 'true' || true,
       },
     }],
   }));
