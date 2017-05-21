93dbcbe2a1560463b74ab422104bc3d96c15d39d
Alvaro Cabrera <pateketrueke@gmail.com>
2017-05-20 22:34:06 -0500

    Improved defaults; WIP

--- bin/tasks/new.js
+++ bin/tasks/new.js
@@ -49,6 +49,30 @@ module.exports = ($, cwd) => {
     throw new Error(`Unsupported DATABASE=${$.data.DATABASE}`);
   }
 
+  /* istanbul ignore else */
+  if ($.data.RELOADER
+    && ['browser-sync', 'live-reload'].indexOf($.data.RELOADER) === -1) {
+    throw new Error(`Unsupported RELOADER=${$.data.RELOADER}`);
+  }
+
+  /* istanbul ignore else */
+  if ($.data.BUNDLER
+    && ['fusebox', 'webpack', 'rollup'].indexOf($.data.BUNDLER) === -1) {
+    throw new Error(`Unsupported BUNDLER=${$.data.BUNDLER}`);
+  }
+
+  /* istanbul ignore else */
+  if ($.data.STYLES
+    && ['less', 'sass', 'styl', 'postcss'].indexOf($.data.STYLES) === -1) {
+    throw new Error(`Unsupported STYLES=${$.data.STYLES}`);
+  }
+
+  /* istanbul ignore else */
+  if ($.data.ES6
+    && ['buble', 'babel', 'traceur'].indexOf($.data.ES6) === -1) {
+    throw new Error(`Unsupported ES6=${$.data.ES6}`);
+  }
+
   const Haki = require('haki');
 
   const haki = new Haki(cwd, _.merge({}, $.flags));
@@ -90,22 +114,41 @@ module.exports = ($, cwd) => {
         $.data.DATABASE === 'mssql' ? 'mssql' : null,
         $.data.DATABASE === 'sqlite' ? 'sqlite3' : null,
         $.data.DATABASE === 'postgres' ? ['pg', 'pg-native'] : null,
-        $.data.DATABASE ? ['sequelize', 'pateketrueke/json-schema-sequelizer'] : null,
+        $.data.DATABASE ? ['sequelize', 'json-schema-sequelizer'] : null,
       ],
       devDependencies: [
         'eslint',
         'eslint-plugin-import',
         'eslint-config-airbnb-base',
         $.data.DATABASE && $.data.DATABASE !== 'sqlite' ? 'sqlite3' : null,
-        ['tarima', 'pug', 'less', 'buble', 'rollup', 'talavera', 'less-plugin-autoprefix'],
+        $.data.BUNDLER === 'fusebox' ? 'fuse-box' : null,
+        $.data.BUNDLER === 'webpack' ? 'webpack' : null,
+        $.data.BUNDLER === 'rollup' ? 'rollup' : null,
+        $.data.STYLES === 'less' ? ['less', 'less-plugin-autoprefix'] : null,
+        $.data.STYLES === 'postcss' ? ['postcss', 'postcss-import', 'postcss-cssnext'] : null,
+        $.data.STYLES === 'sass' ? 'node-sass' : null,
+        $.data.STYLES === 'styl' ? 'styl' : null,
+        $.data.ES6 === 'traceur' ? 'traceur' : null,
+        $.data.ES6 === 'babel' ? ['babel-core', 'babel-preset-es2015', 'babel-plugin-transform-react-jsx'] : null,
+        $.data.ES6 === 'buble' ? 'buble' : null,
+        ['tarima', 'pug', 'talavera'],
       ],
       optionalDependencies: [
-        ['tarima-lr', 'chokidar', 'node-notifier'],
+        $.data.RELOADER === 'browser-sync' ? 'tarima-browser-sync' : null,
+        $.data.RELOADER === 'live-reload' ? 'tarima-lr' : null,
+        ['chokidar', 'node-notifier'],
       ],
     }],
   }, _.merge({
     APP_NAME: name,
+    CSS_LANG: $.data.STYLES,
+    IS_LESS: $.data.STYLES === 'less',
+    IS_BUBLE: $.data.ES6 === 'buble',
+    IS_BABEL: $.data.ES6 === 'babel',
+    IS_ROLLUP: $.data.BUNDLER === 'rollup',
+    IS_POSTCSS: $.data.STYLES === 'postcss',
     IS_SQLITE3: $.data.DATABASE === 'sqlite',
+    IS_BROWSER_SYNC: $.data.RELOADER === 'browser-sync',
   }, $.data))
   .catch(err => {
     _.echo(chalk.red((IS_DEBUG && err.stack) || err.message), '\r\n');
