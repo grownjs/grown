b52e91cce67148141e82c97237870e1f2336bd96
Alvaro Cabrera <pateketrueke@gmail.com>
2017-06-11 02:39:35 -0500

    Fixed route-mappings integration

--- spec/_fixtures/with-middlewares/config/routes.js
+++ spec/_fixtures/with-middlewares/config/routes.js
@@ -1,8 +1,8 @@
 module.exports = routes => {
   return routes()
-    .get('/no', { to: 'Home.index', middleware: 'invalid' })
-    .get('/yes', { to: 'Home.index', middleware: 'test' })
-    .get('/err', { to: 'Home.index', middleware: 'err' })
+    .get('/no', { to: 'Home.index', use: 'invalid' })
+    .get('/yes', { to: 'Home.index', use: 'test' })
+    .get('/err', { to: 'Home.index', use: 'err' })
     .get('/maybe', { to: 'Home.err' })
     .get('/surely', { to: 'Home.test' })
     .get('/other-example', { to: 'Other.main' });
