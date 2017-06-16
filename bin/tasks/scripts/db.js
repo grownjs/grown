ce683cf78ff92c475954e3d9dc989fdce47def57
Alvaro Cabrera <pateketrueke@gmail.com>
2017-06-16 02:32:15 -0500

    Cleanup

--- bin/tasks/scripts/db.js
+++ bin/tasks/scripts/db.js
@@ -9,34 +9,34 @@ module.exports = ($, argv, logger) =>
     }
 
     return Promise.all([
-        _model.count(),
-        _model.describe(),
-      ])
-      .then(results => {
-        logger.info('{% star %s %}\r\n', name);
+      _model.count(),
+      _model.describe(),
+    ])
+    .then(results => {
+      logger.info('{% star %s %}\r\n', name);
 
-        if (argv.flags.schema) {
-          logger.info('%s\n', $.util.inspect(_model.definition.$schema)
-            .replace(/"(.+?)"/g, '{% cyan "$1" %}'));
-          return;
-        }
+      if (argv.flags.schema) {
+        logger.info('%s\n', $.util.inspect(_model.definition.$schema)
+          .replace(/"(.+?)"/g, '{% cyan "$1" %}'));
+        return;
+      }
 
-        if (argv.flags.inspect) {
-          Object.keys(results[1]).forEach(key => {
-            logger.info('  {% item %s %} {% yellow %s %}%s%s%s\n', key,
-              results[1][key].type,
-              results[1][key].allowNull ? '' : ' NOT_NULL',
-              results[1][key].primaryKey ? ' PRIMARY_KEY' : '',
-              results[1][key].defaultValue ? ` {% gray ${results[1][key].defaultValue} %}` : '');
-          });
-          return;
-        }
+      if (argv.flags.inspect) {
+        Object.keys(results[1]).forEach(key => {
+          logger.info('  {% item %s %} {% yellow %s %}%s%s%s\n', key,
+            results[1][key].type,
+            results[1][key].allowNull ? '' : ' NOT_NULL',
+            results[1][key].primaryKey ? ' PRIMARY_KEY' : '',
+            results[1][key].defaultValue ? ` {% gray ${results[1][key].defaultValue} %}` : '');
+        });
+        return;
+      }
 
-        logger.info('  {% gray count: %} %s\n', results[0]);
+      logger.info('  {% gray count: %} %s\n', results[0]);
 
-        logger.info('  {% gray fields: %} %s\n', $.util.inspect((_model.definition.$uiFields || [])
-          .map(prop => prop.field)));
+      logger.info('  {% gray fields: %} %s\n', $.util.inspect((_model.definition.$uiFields || [])
+        .map(prop => prop.field)));
 
-        logger.info('  {% gray references: %} %s\n', $.util.inspect(_model.refs));
-      });
+      logger.info('  {% gray references: %} %s\n', $.util.inspect(_model.refs));
+    });
   }));
