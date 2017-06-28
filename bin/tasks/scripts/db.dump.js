a2aebaaa9c10309e6f9fb1030163b0dcf888ad76
Alvaro Cabrera <pateketrueke@gmail.com>
2017-06-28 13:46:16 -0500

    Fixed dump; add --load support

--- bin/tasks/scripts/db.dump.js
+++ bin/tasks/scripts/db.dump.js
@@ -1,12 +1,33 @@
 'use strict';
 
 const path = require('path');
+const glob = require('glob');
 const fs = require('fs-extra');
 
 module.exports = ($, argv, logger) => {
+  const cwd = $.get('cwd', process.cwd());
+
   return Promise.all((argv._.length ? argv._ : Object.keys($.extensions.models)).map(name => {
     if (!$.extensions.models[name]) {
       throw new Error(`Undefined model ${name}`);
+    }
+
+    if (argv.flags.load) {
+      if (typeof argv.flags.load !== 'string') {
+        throw new Error(`Invalid directory to --load, given '${argv.flags.load}'`);
+      }
+
+      const src = path.join(cwd, argv.flags.load);
+
+      return Promise.all(glob
+        .sync(`**/${name}.json`, { cwd: src })
+        .map(file =>
+          $.extensions.models[name]
+            .bulkCreate(fs.readJsonSync(path.join(src, file)))
+            .catch(e => {
+              logger.info('\r\r{% error Error loading %s (%s) data %}\n', name, file);
+              throw e;
+            })));
     }
 
     return $.extensions.models[name]
@@ -14,15 +35,18 @@ module.exports = ($, argv, logger) => {
       .then(data => ({ data, name }));
   }))
   .then(results => {
-    const cwd = $.get('cwd', process.cwd());
+    if (argv.flags.load) {
+      return;
+    }
+
+    if (argv.flags.save && typeof argv.flags.save !== 'string') {
+      throw new Error(`Invalid directory to --save, given '${argv.flags.save}'`);
+    }
+
     const dest = path.join(cwd, argv.flags.save);
 
     results.forEach(result => {
       if (argv.flags.save) {
-        if (typeof argv.flags.save !== 'string') {
-          throw new Error(`Invalid directory to --save, given '${argv.flags.save}'`);
-        }
-
         const fulldate = [
           new Date().getFullYear(),
           `0${new Date().getMonth() + 1}`.substr(-2),
