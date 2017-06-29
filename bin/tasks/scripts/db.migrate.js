b595c267285336aa60dd92bf6f2101d244f7e436
Alvaro Cabrera <pateketrueke@gmail.com>
2017-06-29 12:40:10 -0500

    Use bundle() from json-schema-sequelizer for exporting schemas

--- bin/tasks/scripts/db.migrate.js
+++ bin/tasks/scripts/db.migrate.js
@@ -4,23 +4,7 @@ const path = require('path');
 const glob = require('glob');
 const fs = require('fs-extra');
 
-function fixRefs(schema) {
-  if (typeof schema === 'object') {
-    if (Array.isArray(schema)) {
-      return schema.map(fixRefs);
-    }
-
-    if (schema.$ref && schema.$ref.indexOf('#/') === -1) {
-      schema.$ref = `#/definitions/${schema.$ref}`;
-    }
-
-    Object.keys(schema).forEach(prop => {
-      schema[prop] = fixRefs(schema[prop]);
-    });
-  }
-
-  return schema;
-}
+const JSONSchemaSequelizer = require('json-schema-sequelizer');
 
 module.exports = ($, argv, logger) => {
   const cwd = $.get('cwd', process.cwd());
@@ -45,25 +29,19 @@ module.exports = ($, argv, logger) => {
     const file = path.join(src, `${fulldate}_${hourtime}${name}.json`);
 
     return logger('write', path.relative(cwd, file), () => {
-      const schemas = {};
-
-      (argv._.length ? argv._ : Object.keys($.extensions.models)).map(model => {
+      const deps = (argv._.length ? argv._ : Object.keys($.extensions.models)).map(model => {
         if (!$.extensions.models[model]) {
           throw new Error(`Undefined model ${model}`);
         }
 
         return $.extensions.models[model];
-      })
-      .forEach(model => {
-        schemas[model.name] = model.options.$schema;
       });
 
-      fs.outputJsonSync(file, {
-        description: typeof argv.flags.snapshot !== 'string'
+      fs.outputJsonSync(file, JSONSchemaSequelizer.bundle(deps,
+        typeof argv.flags.snapshot !== 'string'
           ? new Date().toISOString()
-          : argv.flags.snapshot,
-        definitions: fixRefs(schemas),
-      }, { spaces: 2 });
+          : argv.flags.snapshot)
+      , { spaces: 2 });
     });
   }
 
@@ -74,17 +52,16 @@ module.exports = ($, argv, logger) => {
   }
 
   const models = fs.readJsonSync(path.join(src, all.pop()));
-  // const types = require('json-schema-sequelizer/lib/types');
+  const types = require('json-schema-sequelizer/lib/types');
 
   // FIXME: model this logic inside json-schema-sequelizer; tl-dr
   // re-creation of schemas should be reusable, also the dereferenced
   // schema should not mutate model.options.$schema to keep all $refs as-is
-  console.log(models.definitions);
-  // return Promise.all(Object.keys(models.definitions).map(model => {
-  //   return $.extensions.models[model].sequelize
-  //     .define(model, types.convertSchema(models.definitions[model]).props).sync()
-  //     .then(() => {
-  //       logger.info('\r\r{% log %s was migrated %}\n', model);
-  //     });
-  // }));
+  return Promise.all(Object.keys(models.definitions).map(model => {
+    return $.extensions.models[model].sequelize
+      .define(model, types.convertSchema(models.definitions[model]).props).sync()
+      .then(() => {
+        logger.info('\r\r{% log %s was migrated %}\n', model);
+      });
+  }));
 };
