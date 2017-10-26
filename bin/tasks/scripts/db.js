2ef18f26d197f758cc4086174c3a73b62586fd64
Alvaro Cabrera <pateketrueke@gmail.com>
2017-10-25 23:03:53 -0500

    Fixed db task

--- bin/tasks/scripts/db.js
+++ bin/tasks/scripts/db.js
@@ -3,19 +3,23 @@
 module.exports = ($, argv, logger) => {
   const _extensions = $.extensions('Conn._');
 
-  const dbs = Object.keys(_extensions.dbs);
-
-  return Promise.all(dbs.map(db => {
+  return Promise.all(Object.keys(_extensions.dbs).map(db => {
     logger.info('\r\r{% star %s %}\n', db);
 
     const models = Object.keys(_extensions.dbs[db].sequelize.models)
       .filter(m => (argv._.length ? argv._.indexOf(m) > -1 : true));
 
     return Promise.all(models.map(m => {
-      return Promise.all([
-        _extensions.models[m].count(),
+      const model = _extensions.dbs[db].models[m];
+
+      if (model.virtual) {
+        logger.info('\r\r  {% link %s %} {% gray (virtual) %}\n', m);
+      }
+
+      return !model.virtual && Promise.all([
+        model.count(),
         argv.flags.inspect
-          ? _extensions.models[m].describe()
+          ? model.describe()
           : null,
       ])
       .then(results => {
@@ -24,8 +28,8 @@ module.exports = ($, argv, logger) => {
           results[0],
           results[0] === 1 ? '' : 's');
 
-        Object.keys(_extensions.models[m].associations).forEach(ref => {
-          const refs = _extensions.models[m].associations;
+        Object.keys(model.associations).forEach(ref => {
+          const refs = model.associations;
 
           logger.info('    {% gray %s %} {% yellow %s %} %s\n',
             ref, refs[ref].associationType, refs[ref].target.name);
