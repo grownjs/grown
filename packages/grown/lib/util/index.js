4f3cb74ab4922119b66c4da6c9ce3171c3436d56
Alvaro Cabrera <pateketrueke@gmail.com>
2017-11-12 20:26:07 -0600

    Export utils from object-new; fix

--- packages/grown/lib/util/index.js
+++ packages/grown/lib/util/index.js
@@ -2,6 +2,8 @@
 
 const util = require('util');
 
+const $new = require('object-new');
+
 const objectUtil = require('./object');
 const contextUtil = require('./context');
 
@@ -14,4 +16,9 @@ util._extend(module.exports, contextUtil);
 util._extend(module.exports, {
   buildPipeline,
   buildMiddleware,
+});
+
+// merge definition helpers
+Object.keys($new).forEach(key => {
+  module.exports[key] = $new[key];
 });
