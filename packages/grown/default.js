8d93a56e9eead904680f45d68b5a5a9fc45037c5
Alvaro Cabrera <pateketrueke@gmail.com>
2017-11-20 05:27:45 -0600

    All packages are public; cleanup

--- packages/grown/default.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = (state, h) =>
-  h('html', null,
-    h('body', null,
-      state.contents,
-      state.navigation));
--- packages/grown/view.js
+++
@@ -1,4 +0,0 @@
-const util = require('util');
-
-module.exports = (state, h) =>
-  h('pre', null, util.inspect(state));
