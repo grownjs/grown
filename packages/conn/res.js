1fa7c862207abfa779dd6077c6a0bb72b0e90b7a
Alvaro Cabrera <pateketrueke@gmail.com>
2017-11-19 18:54:57 -0600

    [Conn] Fixed core; add utils

---
+++ packages/conn/res.js
@@ -0,0 +1,228 @@
+'use strict';
+
+const debug = require('debug')('grown:conn');
+
+const statusCodes = require('http').STATUS_CODES;
+const mime = require('mime');
+const send = require('send');
+
+module.exports = ($, util) => {
+  function _finishRequest(ctx, body) {
+    /* istanbul ignore else */
+    if (ctx.res.finished) {
+      throw new Error('Already finished');
+    }
+
+    ctx.res.statusCode = ctx.status_code;
+    ctx.res.statusMessage = statusCodes[ctx.res.statusCode];
+
+    /* istanbul ignore else */
+    if (body && typeof body.pipe === 'function') {
+      debug('#%s Done. Reponse is an stream. Sending as %s', ctx.pid, ctx.content_type);
+
+      /* istanbul ignore else */
+      if (!ctx.res._header) {
+        ctx.res.setHeader('Content-Type', ctx.content_type);
+        ctx.res.writeHead(ctx.res.statusCode);
+      }
+
+      body.pipe(ctx.res);
+
+      return;
+    }
+
+    /* istanbul ignore else */
+    if (body !== null && Buffer.isBuffer(body)) {
+      debug('#%s Response is a buffer. Sending as %s', ctx.pid, ctx.content_type);
+
+      /* istanbul ignore else */
+      if (!ctx.res._header) {
+        ctx.res.setHeader('Content-Type', ctx.content_type);
+        ctx.res.setHeader('Content-Length', body.length);
+      }
+    } else if (body !== null && typeof body === 'object') {
+      debug('#%s Response is an object. Sending as application/json', ctx.pid);
+
+      body = JSON.stringify(body);
+      ctx.content_type = 'application/json';
+    }
+
+    /* istanbul ignore else */
+    if (!ctx.res._header) {
+      ctx.res.setHeader('Content-Type', `${ctx.content_type}; charset=${ctx.resp_charset}`);
+      ctx.res.setHeader('Content-Length', Buffer.byteLength(body || ''));
+      ctx.res.writeHead(ctx.res.statusCode);
+    }
+
+    ctx.res.write(body || '');
+    ctx.res.end();
+  }
+
+  function _endRequest(ctx, code, message) {
+    /* istanbul ignore else */
+    if ((ctx.res && ctx.res.finished) || ctx.halted) {
+      throw new Error('Already finished');
+    }
+
+    let _code = code;
+
+    /* istanbul ignore else */
+    if (typeof code === 'string' || code instanceof Buffer) {
+      _code = ctx.status_code;
+      message = code;
+    }
+
+    if (code instanceof Error) {
+      message = code.message || code.toString();
+      _code = code.statusCode || 500;
+    }
+
+    // normalize output
+    ctx.resp_body = typeof _code === 'string' ? _code : message || ctx.resp_body;
+
+    // normalize response
+    ctx.status_code = typeof _code === 'number' ? _code : ctx.status_code;
+
+    ctx.send(ctx.resp_body);
+  }
+
+  return $.module('Conn.Res', {
+    _finishRequest,
+    _endRequest,
+
+    before_render(ctx, template) {
+      util.extendValues(template.locals, ctx.state);
+    },
+
+    mixins() {
+      const self = this;
+
+      const _response = {
+        headers: Object.create(null),
+        state: Object.create(null),
+        type: 'text/html',
+        body: null,
+        status: null,
+        charset: 'utf8',
+      };
+
+      return {
+        props: {
+          // pipeline status
+          halted: () => _response.body !== null && _response.status !== null,
+
+          // response body
+          has_body: () => _response.body !== null,
+          has_status: () => _response.status !== null,
+
+          get state() {
+            return _response.state;
+          },
+
+          set state(value) {
+            util.extendValues(_response.state, value);
+          },
+
+          get content_type() {
+            return _response.type;
+          },
+
+          set content_type(mimeType) {
+            /* istanbul ignore else */
+            if (!(mimeType && typeof mimeType === 'string')) {
+              throw new Error(`Invalid type: '${mimeType}'`);
+            }
+
+            _response.type = mimeType;
+
+            return this;
+          },
+
+          get status_code() {
+            return _response.status !== null
+              ? _response.status
+              : 200;
+          },
+
+          set status_code(code) {
+            /* istanbul ignore else */
+            if (!(code && statusCodes[code])) {
+              throw new Error(`Invalid set_status: ${code}`);
+            }
+
+            debug('#%s Set status %s', this.pid, code);
+
+            _response.status = code;
+
+            return this;
+          },
+
+          get resp_body() {
+            return _response.body;
+          },
+
+          set resp_body(value) {
+            /* istanbul ignore else */
+            if (!(typeof value === 'string' || typeof value === 'object'
+              || (value && typeof value.pipe === 'function') || (value instanceof Buffer))) {
+              throw new Error(`Invalid resp_body: ${value}`);
+            }
+
+            debug('#%s Set body', this.pid);
+
+            _response.body = value;
+          },
+
+          get resp_charset() {
+            return _response.charset;
+          },
+
+          set resp_charset(value) {
+            _response.charset = value || 'utf8';
+          },
+        },
+        methods: {
+          send_file(entry, mimeType) {
+            /* istanbul ignore else */
+            if (typeof entry === 'object') {
+              mimeType = entry.type || mimeType;
+              entry = entry.file;
+            }
+
+            /* istanbul ignore else */
+            if (!mimeType) {
+              mimeType = mime.getType(entry);
+            }
+
+            const pathname = encodeURI(path.basename(entry));
+
+            const file = send(req, pathname, {
+              root: path.dirname(entry),
+            });
+
+            file.on('headers', _res => {
+              /* istanbul ignore else */
+              if (mimeType) {
+                _res.setHeader('Content-Type', mimeType);
+              }
+            });
+
+            return new Promise((resolve, reject) => {
+              file.on('error', reject);
+              file.on('end', resolve);
+              file.pipe(res);
+            });
+          },
+
+          send(body) {
+            return self._finishRequest(this, body);
+          },
+
+          end(code, message) {
+            return self._endRequest(this, code, message);
+          },
+        },
+      };
+    },
+  });
+};
---
+++ packages/conn/util.js
@@ -0,0 +1,260 @@
+'use strict';
+
+const qs = require('querystring');
+const url = require('url');
+const typeIs = require('type-is');
+const accepts = require('accepts');
+
+module.exports = ($, util) => {
+  function _fixURL(location) {
+    const _uri = url.parse(location);
+
+    let _query = '';
+
+    /* istanbul ignore else */
+    if (_uri.query) {
+      _query = qs.stringify(qs.parse(_uri.query));
+    }
+
+    return [
+      _uri.protocol ? `${_uri.protocol}//` : '',
+      _uri.hostname ? _uri.hostname : '',
+      _uri.port ? `:${_uri.port}` : '',
+      _uri.pathname ? _uri.pathname : '',
+      _query ? `?${_query}` : '',
+    ].join('');
+  }
+
+  return $.module('Conn.Util', {
+    _fixURL,
+
+    mixins() {
+      const self = this;
+
+      function _accepts(req) {
+        if (!_accepts.fn) {
+          _accepts.fn = accepts(req);
+        }
+
+        return _accepts.fn;
+      }
+
+      function _is(req, types) {
+        if (!_is.fn) {
+          _is.fn = typeIs(req);
+        }
+
+        return _is.fn(types);
+      }
+
+      return {
+        props: {
+          resp_headers() {
+            return this.res.getHeaders();
+          },
+
+          req_headers() {
+            return this.req.headers;
+          },
+
+          // ajax
+          is_xhr() {
+            return this.req.headers['x-requested-with'] === 'XMLHttpRequest';
+          },
+
+          is_json() {
+            return typeIs.is(this.req.headers['content-type'], ['json']) === 'json';
+          },
+
+          has_type() {
+            return _is(this.req, Array.prototype.slice.call(arguments));
+          },
+
+          // current connection
+          host() {
+            return (req.headers.host && req.headers.host.split(':')[0]) || process.env.HOST || '0.0.0.0';
+          },
+
+          port() {
+            return (req.headers.host && req.headers.host.split(':')[1]) || process.env.PORT || '8080';
+          },
+
+          remote_ip() {
+            // FIXME:
+            return '0.0.0.0';
+          },
+
+          method() {
+            return req.method;
+          },
+
+          params() {
+            return util.extendValues({}, this.path_params, this.query_params, this.body_params);
+          },
+
+          path_info() {
+            return this.req.url.split('?')[0].split('/').filter(x => x);
+          },
+
+          path_params() {
+            return util.extendValues({}, this.req.params || {});
+          },
+
+          body_params() {
+            return util.extendValues({}, this.req.body || {});
+          },
+
+          request_path() {
+            return this.req.url.split('?')[0];
+          },
+
+          query_string() {
+            return this.req.url.split('?')[1] || '';
+          },
+
+          query_params() {
+            return qs.parse(this.req.url.split('?')[1] || '');
+          },
+
+          accept_charset(charsets) {
+            return _accepts(this.req).charset(charsets);
+          },
+          accept_charsets() {
+            return _accepts(this.req).charsets();
+          },
+
+          accept_encoding(encodings) {
+            return _accepts(this.req).encoding(encodings);
+          },
+          accept_encodings() {
+            return _accepts(this.req).encodings();
+          },
+
+          accept_language(languages) {
+            return _accepts(this.req).language(languages);
+          },
+          accept_languages() {
+            return _accepts(this.req).languages();
+          },
+
+          accept_type(types) {
+            return _accepts(this.req).type(types);
+          },
+          accept_types() {
+            return _accepts(this.req).types();
+          },
+        },
+        methods: {
+          get_req_header(name, defvalue) {
+            /* istanbul ignore else */
+            if (!(name && typeof name === 'string')) {
+              throw new Error(`Invalid req_header: '${name}'`);
+            }
+
+            /* istanbul ignore else */
+            if (typeof req.headers[name] === 'undefined') {
+              return defvalue;
+            }
+
+            return this.req.headers[name];
+          },
+
+          put_req_header(name, value) {
+            /* istanbul ignore else */
+            if (!name || typeof name !== 'string') {
+              throw new Error(`Invalid req_header: '${name}' => '${value}'`);
+            }
+
+            this.req.headers[name] = value;
+
+            return this;
+          },
+
+          delete_req_header(name) {
+            /* istanbul ignore else */
+            if (!(name && typeof name === 'string')) {
+              throw new Error(`Invalid req_header: '${name}'`);
+            }
+
+            delete this.req.headers[name];
+
+            return this;
+          },
+
+          // response headers
+          get_resp_header(name) {
+            return this.res.getHeader(name);
+          },
+
+          put_resp_header(name, value) {
+            /* istanbul ignore else */
+            if (!name || typeof name !== 'string') {
+              throw new Error(`Invalid resp_header: '${name}' => '${value}'`);
+            }
+
+            this.res.setHeader(name, value);
+
+            return this;
+          },
+
+          merge_resp_headers(headers) {
+            /* istanbul ignore else */
+            if (!(headers && (typeof headers === 'object' && !Array.isArray(headers)))) {
+              throw new Error(`Invalid resp_headers: '${headers}'`);
+            }
+
+            Object.keys(headers).forEach(key => {
+              this.put_resp_header(key, headers[key]);
+            });
+
+            return this;
+          },
+
+          delete_resp_header(name) {
+            /* istanbul ignore else */
+            if (!(name && typeof name === 'string')) {
+              throw new Error(`Invalid resp_header: '${name}'`);
+            }
+
+            this.res.removeHeader(name);
+
+            return this;
+          },
+
+          redirect(location, timeout, body) {
+            /* istanbul ignore else */
+            if (!(location && typeof location === 'string')) {
+              throw new Error(`Invalid location: '${location}`);
+            }
+
+            /* istanbul ignore else */
+            if (timeout) {
+              const meta = `<meta http-equiv="refresh" content="${timeout};url=${location}">${body || ''}`;
+
+              return this.end(302, meta);
+            }
+
+            debug('#%s Done. Redirection was found', this.pid);
+
+            return this.put_resp_header('Location', self._fixURL(location)).end(302);
+          },
+
+          raise(code, message) {
+            throw util.buildError(code || 500, message);
+          },
+
+          json(value) {
+            /* istanbul ignore else */
+            if (!value || typeof value !== 'object') {
+              throw new Error(`Invalid JSON value: ${value}`);
+            }
+
+            return this.end(value);
+          },
+        },
+      };
+    },
+  });
+};
+
+
