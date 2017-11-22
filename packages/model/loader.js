3eb40cbf77479a445a809a29e7f5b7a60401b4c2
Alvaro Cabrera <pateketrueke@gmail.com>
2017-11-22 15:14:36 -0600

    [Model.Loader] Add models to the instance

--- packages/model/loader.js
+++ packages/model/loader.js
@@ -1,6 +1,28 @@
 'use strict';
 
 module.exports = ($, util) => {
+  const JSONSchemaSequelizer = require('json-schema-sequelizer');
+
   return $.module('Model.Loader', {
+    scan(cwd) {
+      const db = new JSONSchemaSequelizer(null, [], cwd)
+        .scan((def, model, _sequelize) => {
+          const Model = def($, util.extendValues({}, util, {
+            Sequelize: _sequelize,
+          }));
+
+          Object.keys(def).forEach(key => {
+            util.readOnlyProperty(Model, key, def[key]);
+          });
+
+          return Model;
+        });
+
+      const _refs = util.extendValues({}, db.refs);
+
+      this.extensions.push(_refs);
+
+      return this;
+    },
   });
 };
