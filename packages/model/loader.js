08a9fe6a57f729c8f07204534061c927f879ac7d
Alvaro Cabrera <pateketrueke@gmail.com>
2017-11-22 22:24:28 -0600

    Old way for loading stuff; WIP

--- packages/model/loader.js
+++ packages/model/loader.js
@@ -1,10 +1,13 @@
 'use strict';
 
+const glob = require('glob');
+const path = require('path');
+
 module.exports = ($, util) => {
   const JSONSchemaSequelizer = require('json-schema-sequelizer');
 
   return $.module('Model.Loader', {
-    scan(cwd) {
+    scan(cwd, _refs) {
       const db = new JSONSchemaSequelizer(null, [], cwd)
         .scan((def, model, _sequelize) => {
           const Model = def($, util.extendValues({}, util, {
@@ -18,9 +21,31 @@ module.exports = ($, util) => {
           return Model;
         });
 
-      const _refs = util.extendValues({}, db.refs);
+      const _models = util.extendValues({}, db.refs);
 
-      this.extensions.push(_refs);
+      this.extensions.push(_models);
+
+      /* istanbul ignore else */
+      if (_refs === true) {
+        this.refs(cwd);
+      }
+
+      return this;
+    },
+
+    refs(cwd) {
+      const _schemas = [];
+
+      glob.sync('**/*.json', { cwd }).forEach(json => {
+        /* istanbul ignore else */
+        if (json.indexOf('schema.json') === -1) {
+          _schemas.push(require(path.join(cwd, json)));
+        }
+      });
+
+      this.extensions.push({
+        schemas: _schemas,
+      });
 
       return this;
     },
