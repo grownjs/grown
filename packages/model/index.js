66ba7097e85abf907deda86d791b7424fa73b586
Alvaro Cabrera <pateketrueke@gmail.com>
2017-11-22 23:33:52 -0600

    [Model] Include Resource; fixed namespace

--- packages/model/index.js
+++ packages/model/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
-module.exports = ($, util) => {
-  const Loader = require('./loader')($, util);
-  const Repo = require('./repo')($, util);
+module.exports = (Grown, util) => {
+  const Resource = require('./resource')(Grown, util);
+  const Loader = require('./loader')(Grown, util);
+  const Repo = require('./repo')(Grown, util);
 
-  return $.module('Model', {
+  return Grown.module('Model', {
     include: [
+      Resource,
       Loader,
       Repo,
     ],
