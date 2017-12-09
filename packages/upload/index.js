ef3693fe99344d68c25b4a54902f379919915874
Alvaro Cabrera <pateketrueke@gmail.com>
2017-12-08 22:23:06 -0600

    [Upload] Quick impl

---
+++ packages/upload/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const debug = require('debug')('grown:upload');
+
+module.exports = (Grown, util) => {
+  const IncomingForm = require('formidable').IncomingForm;
+
+  function _processForm(conn, opts) {
+    debug('#%s Processing uploaded files', conn.pid);
+
+    return new Promise((resolve, reject) => {
+      const form = new IncomingForm(util.extendValues({}, opts, defaults));
+
+      form.parse(conn.req, (err, data, files) => {
+        if (err) {
+          debug('#%s Error. The upload was errored: %s', conn.pid, err.message);
+
+          reject(err);
+        } else {
+          debug('#%s Done. The upload was succesfully processed', conn.pid);
+
+          // merge all params, multipart first
+          conn.req.body = util.extendValues({}, data, conn.req.body);
+
+          // expose uploaded files
+          conn.req.files = files;
+
+          // mark as parsed
+          conn.req._body = true;
+
+          resolve();
+        }
+      });
+    });
+  }
+
+  return Grown.module('Upload', {
+    _processForm,
+
+    mixins() {
+      const self = this;
+
+      return {
+        props: {
+          uploaded_files() {
+            return this.req.files || [];
+          },
+        },
+        methods: {
+          upload_files(opts) {
+            return self._processForm(this, opts || {});
+          },
+        },
+      };
+    },
+  });
+};
