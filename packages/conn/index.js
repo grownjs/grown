c5808e2624891869296992d25a753d9d21a5a369
Alvaro Cabrera <pateketrueke@gmail.com>
2017-12-26 01:17:41 -0600

    [Conn.RequestTime] Remove impl; use Logger instead

--- packages/conn/index.js
+++ packages/conn/index.js
@@ -1,13 +1,11 @@
 'use strict';
 
 module.exports = (Grown, util) => {
-  const RequestTime = require('./request-time')(Grown, util);
   const Response = require('./response')(Grown, util);
   const Request = require('./request')(Grown, util);
 
   return Grown('Conn', {
     include: [
-      RequestTime,
       Response,
       Request,
     ],
