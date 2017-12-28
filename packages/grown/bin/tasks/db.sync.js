1157a50c8321dac0316f0ad9029440d0083531a4
Alvaro Cabrera <pateketrueke@gmail.com>
2017-12-28 02:45:08 -0600

    Rename db.reset => db.sync

--- packages/grown/bin/tasks/db.sync.js
+++ packages/grown/bin/tasks/db.sync.js
@@ -2,8 +2,17 @@
 
 const path = require('path');
 
+const USAGE_INFO = `
+
+Reset models from given database
+
+--use   Entry file exporting referenced models
+--sync  Optional. Specific models to reset by name
+
+`;
+
 module.exports = {
-  description: 'Reset all models from database',
+  description: USAGE_INFO,
   callback(Grown, util) {
     if (!Grown.argv.flags.use || typeof Grown.argv.flags.use !== 'string') {
       throw new Error(`Missing models to --use, given '${Grown.argv.flags.use || ''}'`);
@@ -17,11 +26,20 @@ module.exports = {
 
     const options = {
       force: Grown.argv.flags.force === true,
+      alter: Grown.argv.flags.alter === true,
     };
 
     return Promise.resolve()
       .then(() => Models.connect())
-      .then(() => Models.sync(options))
+      .then(() => {
+        if (!Grown.argv.flags.sync) {
+          return Models.sync(options);
+        }
+
+        return Grown.argv.flags.sync.split(',').reduce((prev, cur) => {
+          return prev.then(() => Models[cur].sync(options));
+        }, Promise.resolve());
+      })
       .then(() => Models.disconnect());
   },
 };
