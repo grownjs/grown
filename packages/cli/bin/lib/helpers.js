f9afb3faa7b8ca6d222d6c9e844fd8548316b3fa
Alvaro Cabrera <pateketrueke@gmail.com>
2017-12-30 14:32:22 -0600

    [Grown.CLI] Extract repl helpers; WIP

---
+++ packages/cli/bin/lib/helpers.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = (Grown, util) => ({
+  models(ctx) {
+    const Models = require('./models')(Grown, util);
+
+    return Models.connect()
+      .then(() => {
+        const models = Models._getModels();
+        const count = models.length;
+
+        models.forEach(m => {
+          ctx.repl.context[m.name] = m;
+        });
+
+        const suffix = count === 1 ? '' : 's';
+        const modelNames = models.map(m => m.name).join(', ');
+
+        ctx.logger.printf('{% info %s model%s found: %} %s\r\n', count, suffix, modelNames);
+      });
+  },
+});
--- packages/cli/bin/tasks/repl.js
+++ packages/cli/bin/tasks/repl.js
@@ -18,25 +18,6 @@ module.exports = {
   description: USAGE_INFO,
   callback(Grown, util) {
     Grown.use(require('@grown/repl'));
-
-    Grown.REPL.start({
-      models(ctx) {
-        const Models = require('../lib/models')(Grown, util);
-
-        return Models.connect().then(() => {
-          const models = Models._getModels();
-          const count = models.length;
-
-          models.forEach(m => {
-            ctx.repl.context[m.name] = m;
-          });
-
-          const suffix = count === 1 ? '' : 's';
-          const modelNames = models.map(m => m.name).join(', ');
-
-          ctx.logger.printf('{% info %s model%s found: %} %s\r\n', count, suffix, modelNames);
-        });
-      },
-    });
+    Grown.REPL.start(require('../lib/helpers')(Grown, util));
   },
 };
