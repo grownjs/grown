2d1f6a841cc8f25c9dbf86fa4eff31a6546c7c84
Alvaro Cabrera <pateketrueke@gmail.com>
2018-02-14 03:38:08 -0600

    Reuse bits from bare module; fix

--- packages/grown/index.js
+++ packages/grown/index.js
@@ -2,16 +2,23 @@
 
 const debug = require('debug')('grown');
 
-const path = require('path');
-const _env = require('dotenv');
+const _util = require('util');
 const wargs = require('wargs');
 const $new = require('object-new');
 
 const _pkg = require('./package.json');
 
-const util = require('./lib/util');
+const util = require('@grown/bare/util');
 const _mount = require('./lib/mount');
 const _listen = require('./lib/listen');
+
+const buildPipeline = require('./lib/plug/pipeline');
+const buildMiddleware = require('./lib/plug/middleware');
+
+_util._extend(util, {
+  buildPipeline,
+  buildMiddleware,
+});
 
 require('source-map-support').install();
 
@@ -36,8 +43,6 @@ require('global-or-local')
     'pg',
     'uws',
   ]);
-
-const RE_UPPER = /^[A-Z][A-Z_]*$/;
 
 let _pid = 0;
 
@@ -253,57 +258,18 @@ module.exports = (cwd, argv) => {
     },
   });
 
-  // defaults
-  process.name = 'Grown';
-
-  process.env.HOST = _argv.flags.host || process.env.HOST || '0.0.0.0';
-  process.env.PORT = _argv.flags.port || process.env.PORT || 8080;
-
-  process.env.NODE_ENV = _argv.flags.env || process.env.NODE_ENV || 'development';
-
-  Object.keys(_argv.data).forEach(key => {
-    /* istanbul ignore else */
-    if (RE_UPPER.test(key)) {
-      process.env[key] = _argv.data[key];
-    }
-  });
-
-  /* istanbul ignore else */
-  if (process.env.CI && process.env.NODE_ENV.indexOf('test') === 0) {
-    process.env.NODE_ENV = 'ci';
-  }
-
-  delete _argv.flags.env;
-
-  /* istanbul ignore else */
-  if (_argv.flags.debug && _argv.flags.verbose) {
-    require('debug').enable('*');
-  }
-
   // private container
-  function $(id, props, extensions) {
+  const $ = function $(id, props, extensions) {
     return $new(id, props, $, extensions);
-  }
+  };
 
   const Grown = $('Grown', grownFactory.bind(null, $));
 
-  $('Grown.version', () => _pkg.version, false);
-  $('Grown.argv', () => _argv, false);
-  $('Grown.cwd', () => cwd || process.cwd(), false);
-  $('Grown.env', () => process.env.NODE_ENV, false);
-  $('Grown.use', cb => cb(Grown, util), false);
+  // defaults
+  process.name = 'Grown (bare)';
 
-  const env = _env.config({
-    path: path.join(Grown.cwd, '.env'),
-  });
-
-  if (env.error && env.error.code !== 'ENOENT') {
-    throw env.error;
-  }
-
-  // cleanup
-  delete process.env.error;
-  delete env.error;
+  require('@grown/bare/environment')(_argv);
+  require('@grown/bare/configure')($, cwd || process.cwd(), _argv, Grown);
 
   return Grown;
 };
