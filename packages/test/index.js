a04c9e8079162d73a8213c39a8bfd3763414e2f3
Alvaro Cabrera <pateketrueke@gmail.com>
2018-02-14 02:05:32 -0600

    Quick impl for Grown.Test.do() helper

--- packages/test/index.js
+++ packages/test/index.js
@@ -4,10 +4,24 @@ module.exports = (Grown, util) => {
   const Request = require('./request')(Grown, util);
   const Mock = require('./mock')(Grown, util);
 
+  Grown.use(require('@grown/logger'));
+
+  const logger = Grown.Logger.getLogger();
+
   return Grown('Test', {
     include: [
       Request,
       Mock,
     ],
+
+    do(callback) {
+      return done =>
+        Promise.resolve()
+          .then(() => callback())
+          .catch(e => {
+            logger.printf('\r\r%s\n', util.cleanError(e).stack || e.toString());
+          })
+          .then(() => typeof done === 'function' && done());
+    },
   });
 };
