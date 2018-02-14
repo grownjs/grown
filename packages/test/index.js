e42edc2a2586be8c3a3758c517bbe441f70ecd3a
Alvaro Cabrera <pateketrueke@gmail.com>
2018-02-14 03:58:45 -0600

    Remove do() helper; too specific for jasmine-node

--- packages/test/index.js
+++ packages/test/index.js
@@ -4,33 +4,10 @@ module.exports = (Grown, util) => {
   const Request = require('./request')(Grown, util);
   const Mock = require('./mock')(Grown, util);
 
-  Grown.use(require('@grown/logger'));
-
-  const logger = Grown.Logger.getLogger();
-
   return Grown('Test', {
     include: [
       Request,
       Mock,
     ],
-
-    do(callback) {
-      return done => {
-        function end(ex) {
-          if (typeof done === 'function') {
-            done(ex);
-          }
-
-          if (ex) {
-            logger.printf('\r\r%s\n', util.cleanError(ex).stack || ex.toString());
-          }
-        }
-
-        return Promise.resolve()
-          .then(() => callback())
-          .then(() => end())
-          .catch(end);
-      };
-    },
   });
 };
