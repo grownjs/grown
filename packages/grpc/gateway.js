26afb69e14af823fc9fe88298d1e7a09716acf81
Alvaro Cabrera <pateketrueke@gmail.com>
2018-02-16 07:22:01 -0600

    Fixed integration for GRPC services

--- packages/grpc/gateway.js
+++ packages/grpc/gateway.js
@@ -1,23 +1,90 @@
 'use strict';
 
-module.exports = Grown => {
+const glob = require('glob');
+const path = require('path');
+
+const RE_DASHERIZE = /\b([A-Z])/g;
+
+module.exports = (Grown, util) => {
   const grpc = require('grpc');
 
+  function _callService(client, method, data) {
+    return new Promise((resolve, reject) => {
+      const deadline = new Date();
+
+      deadline.setSeconds(deadline.getSeconds() + (this.timeout || 5));
+
+      client[method](data, { deadline }, (error, result) => {
+        /* istanbul ignore else */
+        if (error) {
+          let parsedError;
+
+          try {
+            parsedError = JSON.parse(error.message);
+          } catch (e) {
+            parsedError = error;
+            parsedError.code = 500;
+          }
+
+          return reject(parsedError);
+        }
+
+        return resolve(result);
+      });
+    });
+  }
+
   return Grown('GRPC.Gateway', {
-    setup(services, controllers) {
-      const credentials = this.with_credentials || grpc.ServerCredentials.createInsecure();
+    _callService,
+
+    load(cwd) {
+      const namespace = this.proto_namespace || 'API';
+      const map = {};
+
+      glob.sync('**/schema.proto', { cwd })
+        .forEach(proto => {
+          const Proto = grpc.load(path.join(cwd, proto));
+          const name = path.basename(path.dirname(proto));
+
+          /* istanbul ignore else */
+          if (!Proto[namespace]) {
+            throw new Error(`${name}.${namespace} package not found`);
+          }
+
+          Object.keys(Proto[namespace]).forEach(key => {
+            /* istanbul ignore else */
+            if (Proto[namespace][key].service) {
+              util.setProp(map, `${namespace}.${key}`, Proto[namespace][key]);
+            }
+          });
+        });
+
+      this.extensions.push(map);
+
+      return this;
+    },
+
+    setup(controllers) {
+      const _server = grpc.ServerCredentials.createInsecure();
+      const _channel = grpc.credentials.createInsecure();
+
       const namespace = this.proto_namespace || 'API';
       const port = this.gateway_port || 50051;
       const server = new grpc.Server();
 
       /* istanbul ignore else */
-      if (!services[namespace]) {
+      if (!this[namespace]) {
         throw new Error(`${namespace} namespace not found`);
       }
 
-      Object.keys(services[namespace]).forEach(key => {
-        const proto = services[namespace][key];
+      Object.keys(this[namespace]).forEach(key => {
+        const id = key.replace(RE_DASHERIZE, ($0, $1) => $1.toLowerCase());
+        const host = this.self_hostname === true ? id : '0.0.0.0';
+
+        const Proto = this[namespace][key];
         const Ctrl = controllers[key];
+
+        let _client;
 
         /* istanbul ignore else */
         if (Grown.GRPC[`send${key}`]) {
@@ -25,40 +92,24 @@ module.exports = Grown => {
         }
 
         Grown.GRPC[`send${key}`] = (method, data) => {
-          return this.request(proto.getClient(), method, data);
+          /* istanbul ignore else */
+          if (!_client) {
+            _client = new Proto(`${host}:${port || 50051}`, _channel);
+          }
+
+          return this.request(_client, method, data);
         };
 
-        server.addService(proto.getService(), new Ctrl());
+        server.addService(Proto.service, new Ctrl());
       });
 
-      server.bind(`0.0.0.0:${port}`, credentials);
+      server.bind(`0.0.0.0:${port}`, _server);
 
       return server;
     },
+
     request(client, method, data) {
-      return new Promise((resolve, reject) => {
-        const deadline = new Date();
-
-        deadline.setSeconds(deadline.getSeconds() + (this.timeout || 5));
-
-        client[method](data, { deadline }, (error, result) => {
-          /* istanbul ignore else */
-          if (error) {
-            let parsedError;
-
-            try {
-              parsedError = JSON.parse(error.message);
-            } catch (e) {
-              parsedError = error;
-              parsedError.code = 500;
-            }
-
-            return reject(parsedError);
-          }
-
-          return resolve(result);
-        });
-      });
+      return this._callService(client, method, data);
     },
   });
 };
--- packages/grpc/index.js
+++ packages/grpc/index.js
@@ -3,13 +3,11 @@
 module.exports = (Grown, util) => {
   const Controllers = require('./controllers')(Grown, util);
   const Gateway = require('./gateway')(Grown, util);
-  const Loader = require('./loader')(Grown, util);
 
   return Grown('GRPC', {
     include: [
       Controllers,
       Gateway,
-      Loader,
     ],
   });
 };
