8f8e2fa7d543e4cf278bb89dd0943f25ea8eb558
Alvaro Cabrera <pateketrueke@gmail.com>
2018-02-17 14:39:05 -0600

    Use logger instance if present; fix

--- packages/bare/configure.js
+++ packages/bare/configure.js
@@ -9,13 +9,19 @@ module.exports = ($, cwd, argv, Grown) => {
   const _loader = (cwd, suffix) => util.scanDir(cwd, suffix, cb => cb(Grown, util));
   const _pkg = require(path.join(cwd, 'package.json'));
 
+  function getLogger() {
+    if (Grown.Logger) {
+      return msg => Grown.Logger.getLogger().printf('\r\r{% error %s %}\n', msg);
+    }
+  }
+
   $('Grown.version', () => _pkg.version, false);
   $('Grown.loader', () => _loader, false);
   $('Grown.argv', () => argv, false);
   $('Grown.cwd', () => cwd, false);
   $('Grown.env', () => process.env.NODE_ENV, false);
   $('Grown.use', cb => cb(Grown, util), false);
-  $('Grown.do', cb => util.wrap(cb), false);
+  $('Grown.do', cb => util.wrap(cb, getLogger), false);
 
   const env = _env.config({
     path: path.join(cwd, '.env'),
