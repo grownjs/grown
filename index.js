f071593ba653c380b702afda70254416f89043fa
Alvaro Cabrera <pateketrueke@gmail.com>
2018-03-08 02:31:24 -0600

    Use bud; fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = (cwd, argv) => {
-  const Grown = require('@grown/bare')(cwd, argv);
+  const Grown = require('@grown/bud')(cwd, argv);
 
   Grown.use(require('@grown/server'));
 
