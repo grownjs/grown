1b960fcd29b1c3b057a404306ecf1febac7d7a08
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-03-19 21:12:44 -0600

    Bootstraip; setup CLI

---
+++ cli.js
@@ -0,0 +1,10 @@
+const Grown = require('@grown/bud')();
+
+Grown.use(require('@grown/cli'));
+
+if (Grown.argv.flags.version) {
+  Grown.Logger.getLogger()
+    .printf('\r{% green Grown v%s %}\r\n', Grown.version);
+} else {
+  Grown.CLI.start(Grown.argv._.shift());
+}
