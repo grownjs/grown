1ab769a26399840dc21665e13014872516b58893
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-03-23 01:31:11 -0600

    Add DB.bundle() for loading models; fix

--- packages/model/db.js
+++ packages/model/db.js
@@ -27,5 +27,41 @@ module.exports = (Grown, util) => {
 
       return this._registry;
     },
+
+    bundle(options) {
+      options = options || {};
+
+      const name = (options.database && options.database.identifier) || 'default';
+      const DB = Grown.Model.DB.register(name, options.database);
+      const Models = Grown.load(options.models, {
+        before(_name, definition) {
+          DB[name].add(definition);
+        },
+        after(_name) {
+          return DB[name].models[_name];
+        },
+      });
+
+      const map = {};
+
+      Object.keys(DB[name].$refs).forEach(ref => {
+        if (DB[name].$refs[ref].$schema.properties) {
+          Object.defineProperty(map, ref, {
+            enumerable: true,
+            configurable: false,
+            get: () => Models.get(ref),
+          });
+        }
+      });
+
+      return Grown(`Model.DB.$${name}`, {
+        _getDB: _name => DB[_name],
+        getModel: _name => Models.get(_name),
+        connect: DB[name].connect,
+        disconnect: DB[name].close,
+        connection: DB[name].sequelize.options,
+        include: [map],
+      });
+    },
   });
 };
