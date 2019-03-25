631054498d4699d1e84e0ead16ada8e0a4f38480
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-03-25 12:29:04 -0600

    Cleanup

--- application/lib/index.js
+++ application/lib/index.js
@@ -1,11 +1,8 @@
-const fs = require('fs');
 const path = require('path');
 const Application = require('@grown/bud')();
 
 const GRPC = Application.use(require('@grown/grpc'));
 const Models = Application.use(require('./models'));
-
-const mailerInstance = require('./mailer');
 
 Application('GRPC.Gateway', {
   include: [
@@ -18,7 +15,7 @@ Application('Services', {
     GRPC.Gateway.setup(Application.load(path.join(__dirname, '../api/handlers'))),
   ],
   getMailer() {
-    return mailerInstance;
+    return require('./mailer');
   },
 });
 
