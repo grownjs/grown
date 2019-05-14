9d1afdd7fee6519d229029b88dd14fe710916079
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-05-14 01:18:47 -0500

    Cleanup

--- application/web/front/shared/graphql.js
+++ application/web/front/shared/graphql.js
@@ -1,16 +1,59 @@
-import graphqlClient from '../lib/graphql-client';
+import FetchQL from 'fetchql';
+import { session, conn } from '../shared/stores';
 
-let session;
+class GraphQLClient {
+  constructor(url, options) {
+    const client = new FetchQL({
+      url,
+      onStart(x) { conn.set({ loading: x > 0 }); },
+      onEnd(x) { conn.set({ loading: x > 0 }); },
+      ...options,
+    });
 
-try {
-  session = JSON.parse(localStorage.getItem('session')) || {};
-} catch (e) {
-  session = {};
+    function resp(result, callback) {
+      const retval = typeof callback === 'function' && callback(result.data);
+
+      if (!retval && result.data) {
+        session.update(old => Object.assign(old, result.data));
+      }
+
+      return retval;
+    }
+
+    function query(gql, data, callback) {
+      if (typeof data === 'function') {
+        callback = data;
+        data = undefined;
+      }
+
+      return client
+        .query({ query: gql, variables: data })
+        .then(result => resp(result, callback));
+    }
+
+    function mutation(gql, cb = done => done()) {
+      return function call$(...args) { cb((data, callback) => query(gql, data, callback)).apply(this, args); };
+    }
+
+    return {
+      client,
+      query,
+      mutation,
+    };
+  }
 }
 
-const $ = graphqlClient('/api/v1/graphql', {
+let _session;
+
+try {
+  _session = JSON.parse(localStorage.getItem('session')) || {};
+} catch (e) {
+  _session = {};
+}
+
+const $ = new GraphQLClient('/api/v1/graphql', {
   headers: {
-    Authorization: `Bearer ${session.token}`,
+    Authorization: `Bearer ${_session.token}`,
   },
 });
 
