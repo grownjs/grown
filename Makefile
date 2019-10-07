0dc298d0ef7c406e526587b08f5d5246a76d15db
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-10-07 09:35:41 -0500

    Cleanup

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@ test:
 	@npm run lerna bootstrap
 	@npm run lerna run -- test:ci --scope @grown/bud
 	@npm run lerna run -- test:ci --scope @grown/cli
-	@npm run lerna run -- test:ci --scope @grown/conn
 	@npm run lerna run -- test:ci --scope @grown/server
 	@npm run lerna run -- test:ci --scope @grown/parsers
+	@npm run lerna run -- test:ci --scope @grown/conn
 
 clean:
 	@npm run lerna clean
--- packages/cli/tests/main.test.js
+++ packages/cli/tests/main.test.js
@@ -8,51 +8,51 @@ const { cmd, bin } = require('./helpers');
 describe('Grown.CLI', () => {
   it('should handle no-package.json', () => {
     return cmd('empty', bin()).then(({ stdout, stderr, failure }) => {
-      expect(failure).to.be.null;
       expect(stderr).to.eql('');
       expect(stdout).to.contain('Add --help to any command for usage info.');
+      expect(failure).to.be.null;
     });
   });
 
   it('should call wrapped command', () => {
     return cmd('sample', bin()).then(({ stdout, stderr, failure }) => {
-      expect(failure).to.be.null;
       expect(stderr).to.eql('');
       expect(stdout).to.contain('Add --help to any command for usage info.');
+      expect(failure).to.be.null;
     });
   });
 
   describe('Tasks', () => {
     it('should list found tasks', () => {
       return cmd('sample', bin('example --help')).then(({ stdout, stderr, failure }) => {
-        expect(failure).to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('This line will be shown as summary');
         expect(stdout).to.contain('And this description is shown when --help is given.');
+        expect(failure).to.be.null;
       });
     });
 
     it('should be able to run tasks', () => {
       return cmd('sample', bin('example')).then(({ stdout, stderr, failure }) => {
-        expect(failure).to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('OSOM');
+        expect(failure).to.be.null;
       });
     });
 
     it('should report on unknown tasks', () => {
       return cmd('sample', bin('undefined')).then(({ stdout, stderr, failure }) => {
-        expect(failure).not.to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('Task undefined is not registered');
+        expect(failure).not.to.be.null;
       });
     });
 
     it('should report on failed tasks', () => {
       return cmd('sample', bin('broken')).then(({ stdout, stderr, failure }) => {
-        expect(failure).not.to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('[broken] (Error) Unexpected');
+        expect(failure).not.to.be.null;
       });
     });
   });
@@ -60,53 +60,53 @@ describe('Grown.CLI', () => {
   describe('Errors', () => {
     it('should report multiple errors', () => {
       return cmd('errored', bin('up --app multiple')).then(({ stdout, stderr, failure }) => {
-        expect(failure).not.to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('─ SINGLE_ERROR (undefined)');
         expect(stdout).to.contain('─ [up] Error');
+        expect(failure).not.to.be.null;
       });
     });
 
     it('should report original errors', () => {
       return cmd('errored', bin('up --app original')).then(({ stdout, stderr, failure }) => {
-        expect(failure).not.to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('⚠ SOME DETAIL INFO');
         expect(stdout).to.contain('⚠ ORIGINAL_ERROR');
+        expect(failure).not.to.be.null;
       });
     });
 
     it('should report stack-less errors', () => {
       return cmd('errored', bin('up --app no-stack --debug')).then(({ stdout, stderr, failure }) => {
-        expect(failure).not.to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('─ [up] NO_STACK');
+        expect(failure).not.to.be.null;
       });
     });
 
     it('should handle subtasks', () => {
       return cmd('sample', bin('subtest')).then(({ stdout, stderr, failure }) => {
-        expect(failure).to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('subtest');
         expect(stdout).to.contain('DONE');
+        expect(failure).to.be.null;
       });
     });
 
     it('should report rejections', () => {
       return cmd('sample', bin('subtest throw')).then(({ stdout, stderr, failure }) => {
-        expect(failure).not.to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('subtest');
         expect(stdout).to.contain('─ (Error) UNHANDLED_REJECTION');
+        expect(failure).not.to.be.null;
       });
     });
 
     it('should report errors', () => {
       return cmd('sample', bin('subtest error')).then(({ stdout, stderr, failure }) => {
-        expect(failure).not.to.be.null;
         expect(stdout).to.contain('subtest');
         expect(stderr).to.contain('FAILED');
+        expect(failure).not.to.be.null;
       });
     });
   });
@@ -116,39 +116,38 @@ describe('Grown.CLI', () => {
 
     it('should handle custom commands', () => {
       return cmd('sample', bin(`example -- node ${TEST_JS}`)).then(({ stdout, stderr, failure }) => {
-        expect(failure).to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('DONE');
         expect(stdout).to.contain('OSOM');
         expect(stdout).to.contain('example');
         expect(stdout).to.contain('Done');
+        expect(failure).to.be.null;
       });
     });
 
     it('should report missed-rejections', () => {
       return cmd('sample', bin(`example -- node ${TEST_JS} throw`)).then(({ stdout, stderr, failure }) => {
-        expect(failure).to.be.null;
         expect(stderr).to.include('Error: UNHANDLED_REJECTION');
         expect(stdout).to.contain('OSOM');
         expect(stdout).to.contain('example');
         expect(stdout).to.contain('Done');
+        expect(failure).to.be.null;
       });
     });
 
     it('should report SIGINT-calls', () => {
       return cmd('sample', bin(`example -- node ${TEST_JS} wait`), true).then(({ stdout, stderr, failure }) => {
-        expect(failure).to.be.null;
         expect(stderr).to.eql('');
-        expect(stdout).not.to.eql('');
         expect(stdout).not.to.contain('DONE');
+        expect(failure).to.be.null;
       });
     });
 
     it('should report trap errors', () => {
       return cmd('sample', bin(`example -- node ${TEST_JS} error`)).then(({ stdout, stderr, failure }) => {
-        expect(failure).not.to.be.null;
         expect(stderr).to.contain('FAILED');
         expect(stdout.split('\n').length).to.eql(2);
+        expect(failure).not.to.be.null;
       });
     });
   });
@@ -156,34 +155,34 @@ describe('Grown.CLI', () => {
   describe('Application', () => {
     it('should fail if --app is not found', () => {
       return cmd('sample', bin('up')).then(({ stdout, stderr, failure }) => {
-        expect(failure).not.to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('Missing application script');
+        expect(failure).not.to.be.null;
       });
     });
 
     it('should fail if --app is empty', () => {
       return cmd('app', bin('up')).then(({ stdout, stderr, failure }) => {
-        expect(failure).not.to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('[up] (TypeError) serverFactory is not a function');
+        expect(failure).not.to.be.null;
       });
     });
 
     it('should fail if --app is broken', () => {
       return cmd('app', bin('up --app broken')).then(({ stdout, stderr, failure }) => {
-        expect(failure).not.to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain("[up] (TypeError) Cannot read property 'listen' of undefined");
+        expect(failure).not.to.be.null;
       });
     });
 
     it('should allow custom --app', () => {
       return cmd('app', bin('up --app working')).then(({ stdout, stderr, failure }) => {
-        expect(failure).to.be.null;
         expect(stderr).to.eql('');
         expect(stdout).to.contain('WORKING...');
         expect(stdout).to.contain('LISTENING...');
+        expect(failure).to.be.null;
       });
     });
   });
