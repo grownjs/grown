56c529c0689b2e89bf60b1fc951c747504f462d5
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-10-19 04:47:58 -0500

    [Test] Add quick helper for req.query

--- packages/test/request.js
+++ packages/test/request.js
@@ -59,6 +59,24 @@ module.exports = Grown => {
     _fixRequest,
 
     $install(ctx) {
+      // quick middlewares
+      ctx.mount(conn => {
+        if (!conn.req.query) {
+          const query = conn.req.url.split('?')[1] || '';
+          const parts = query.split('&');
+
+          conn.req.query = {};
+
+          parts.forEach(item => {
+            const [key, value] = item.split('=');
+
+            if (key.length) {
+              conn.req.query[key] = value;
+            }
+          });
+        }
+      });
+
       return {
         methods: {
           request(url, method, options, callback) {
