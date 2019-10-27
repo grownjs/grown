7333172cd12b5b9260d8802080160ee5e0e1c2a7
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-10-27 11:25:13 -0600

    Fixed docker setup

--- application/api/config.js
+++ application/api/config.js
@@ -2,6 +2,6 @@ module.exports = {
   facebook: {
     clientID: process.env.FB_CLIENT_ID,
     clientSecret: process.env.FB_CLIENT_SECRET,
-    callbackURL: 'http://localhost:4001/auth/facebook/callback',
+    callbackURL: `${process.env.BASE_URL || 'http://localhost:4001'}/auth/facebook/callback`,
   },
 };
--- application/e2e/cases/home.test.js
+++ application/e2e/cases/home.test.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 import config from '../config.js';
 
-fixture.skip('Home')
+fixture('Home')
   .page(config.url);
 
 test('should start the application', async t => {
