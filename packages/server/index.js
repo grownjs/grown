f3a8cca194eb2e4322ad1d170f264cc455a474ed
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-05-11 22:58:19 -0500

    Fixed test and coverage setup

--- packages/server/index.js
+++ packages/server/index.js
@@ -133,7 +133,8 @@ function _grownFactory($, util, options) {
       if (options.cors) {
         _mount.call(scope, (req, res, next) => {
           res.setHeader('Access-Control-Allow-Origin', '*');
-          res.setHeader('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+          res.setHeader('Access-Control-Allow-Headers',
+            'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
           res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
           res.setHeader('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
