ab55a313725b4d7132e247d145ada631b63b746d
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-05-24 20:56:04 -0500

    Upgrade mailor

--- application/lib/mailer/index.js
+++ application/lib/mailer/index.js
@@ -2,5 +2,5 @@ const path = require('path');
 const Mailor = require('mailor');
 
 module.exports = Mailor.buildMailer(path.join(__dirname, 'generated'), {
-  maildev: ['test', 'development'].includes(process.env.NODE_ENV) || process.env.MAILDEV === 'YES',
+  maildev: ['test', 'development'].includes(process.env.NODE_ENV) && process.env.MAILDEV === 'YES',
 });
