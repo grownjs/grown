12f2b4aa2f086197be87bef8d6ea051299ea918a
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-06-26 11:29:28 -0500

    Upgrade mailor for multi-support

--- application/lib/mailer.js
+++ application/lib/mailer.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const Mailor = require('mailor');
 
-module.exports = Mailor.buildMailer(path.join(__dirname, 'generated'), {
+module.exports = Mailor.buildMailer(path.join(__dirname, '../etc/mailer/generated'), {
   maildev: ['test', 'development'].includes(process.env.NODE_ENV) && process.env.MAILDEV === 'YES',
 });
