d94f4813880381770ebc39ea44ecfdebfcb33684
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-07-03 10:33:12 -0500

    Bind well-known modules if available

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 module.exports = (cwd, argv) => {
   const Grown = require('@grown/bud')(cwd, argv);
 
   Grown.use(require('@grown/server'));
+
+  const dir = path.join(process.cwd(), 'node_modules/@grown');
+
+  if (fs.existsSync(dir)) {
+    Grown.bind('@grown/', dir);
+  }
 
   return Grown;
 };
