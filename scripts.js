93b7a49cbcdb69398cf6b26e2808d37f58cc6958
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-09-12 16:42:18 -0500

    Release: Sat Sep 12 16:42:17 CDT 2020

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
 
+function links(baseURL) {
+  if (typeof target === 'undefined') { return; }
+  target.style.display = 'block';
+  [].slice.call(document.querySelectorAll('a>code')).forEach(function (node) {
+    if (!node.dataset.href) { node.dataset.href = node.parentNode.href.replace(location.origin, ''); }
+    node.parentNode.setAttribute('target', 'external');
+    node.parentNode.setAttribute('href', baseURL + node.dataset.href);
+  });
+}
 [].slice.call(document.querySelectorAll('pre code.lang-js')).forEach(function (source) {
   var matches = source.innerText.match(/\/\*+\s*@runkit\s*(.+?)\s*\*+\//);
   if (!matches) { return; }
@@ -13,17 +22,20 @@
     a._locked = true;
     delete a.onclick;
     e.preventDefault();
-    var target = document.createElement('div');
-    source.parentNode.parentNode.insertBefore(target, source.parentNode);
+    var el = document.createElement('div');
+    source.parentNode.parentNode.insertBefore(el, source.parentNode);
     source.parentNode.parentNode.removeChild(source.parentNode);
-    RunKit.createNotebook({
-      element: target,
+    var notebook = RunKit.createNotebook({
+      element: el,
       source: sourceCode,
       mode: isEndpoint && 'endpoint',
       title: snippet.title || 'Untitled',
-      preamble: snippet.preamble,
+      preamble: snippet.preamble
+        + (isEndpoint ? '\nexports.endpoint=(req,res)=>{res.end()}' : ''),
+      environment: [{ name: 'U_WEBSOCKETS_SKIP', value: 'true' }],
       gutterStyle: 'none',
       evaluateOnLoad: true,
+      onURLChanged: function () { return notebook.getEndpointURL().then(links); },
     });
   };
   source.parentNode.appendChild(a);
