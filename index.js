1b47fc28c1c99b2a31c5abe22b4a26f451dbe3e2
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-12-27 22:51:45 -0600

    cleanup

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = (cwd, argv) => {
   const Grown = require('@grown/bud')(cwd, argv);
 
   Grown.use(require('@grown/server'));
+  Grown.use(require('@grown/conn'));
 
   return Grown;
 };
