05a4262c784d6451198afafc6905996b1b9dabba
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-12-28 23:32:00 -0600

    chore: conn is optional

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = (cwd, argv) => {
   const Grown = require('@grown/bud')(cwd, argv);
 
   Grown.use(require('@grown/server'));
-  Grown.use(require('@grown/conn'));
 
   return Grown;
 };
