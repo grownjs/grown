896df5f84419043278849b9f553b5ef87d6bf644
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2021-01-17 19:12:40 -0600

    lock deps

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -218,7 +218,7 @@ module.exports = (Grown, util) => {
           env: {
             ...process.env,
             ...environment,
-          }
+          },
         });
 
         // clear previous logs...
