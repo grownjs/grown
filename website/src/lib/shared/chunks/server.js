917df3399fd82074291d9d70a26431df9ec059ad
Alvaro Cabrera <pateketrueke@gmail.com>
2022-01-22 04:30:50 -0600

    fix: simplified server chunk

--- website/src/lib/shared/chunks/server.js
+++ website/src/lib/shared/chunks/server.js
@@ -1,9 +1,11 @@
 /* eslint-disable */
 
-Grown = require('@grown/bud')();
 Grown.use(require('@grown/server'));
 
+prefix = 'app';
 server = new Grown();
 
 process.nextTick(() => server.listen(8080));
 process.stdout.write = msg => console.log(msg);
+
+eval(`__dirname='${path.join(process.cwd(), 'app')}';`);
